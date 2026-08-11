9dd2ea80adf1019fca088523782551987f19de27
abdullabaki <your_email@example.com>
2026-08-11 12:49:41 +0600

    new in signup

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -4,16 +4,21 @@ import { Link } from "react-router";
 const Signup = () => {
    return (
       <div className="pt-30">
-         <div className="h-180 w-7xl mx-auto rounded-xl shadow-xl/30 bg-[#EDEDED] grid grid-cols-2 gap-6 p-8">
-            <div className="flex items-center justify-center h-full w-full text-primary-content bg-primary rounded-2xl shadow-xl/30">
+         <div className="h-180 w-7xl mx-auto rounded-xl shadow-xl/30 border-2 border-[#EDEDED] grid grid-cols-2 gap-6 p-8">
+            <div className="flex items-center justify-center h-full w-full text-primary-content rounded-2xl shadow-xl/30 bg-linear-to-b from-primary to-secondary">
                <div className="text-center w-80">
-                  <h3>Welcome Back</h3>
-                  <h2>Hello, Friend</h2>
-                  <p>
-                     Your trusted partner for mobile and laptop care. Log in to access your repair records, real-time updates, and service invoices instantly.
+                  <h3 className="glass inline px-4 py-2 rounded-3xl">Welcome Back</h3>
+                  <h2 className="pt-4 font-bold text-3xl pb-3">Hello, Friend!</h2>
+                  <p className="text-xs font-light pb-1">
+                     Your trusted partner for mobile and laptop care. Log in to
+                     access your repair records, real-time updates, and service
+                     invoices instantly.
+                  </p>
+                  <p className="font-medium pb-3">
+                     Already have account ?
                   </p>
                   <Link to="/signin">
-                     <button>Sign In</button>
+                     <button className="glass inline px-8 py-2 rounded-3xl font-medium">Sign In</button>
                   </Link>
                </div>
             </div>
